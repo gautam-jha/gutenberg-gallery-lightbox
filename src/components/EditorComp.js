d79bd861ec082117531227626004a0cd843f6fc5
Gautam Jha <gautamjha.mailbox@gmail.com>
2021-08-30 22:12:44 +0530

    Updated Block - Image remove logics

--- src/components/EditorComp.js
+++ src/components/EditorComp.js
@@ -1,13 +1,51 @@
-function EditorComp(props) {
+const { MediaUpload, MediaUploadCheck } = wp.blockEditor;
+const { Button } = wp.components;
+const { __ } = wp.i18n;
 
-	const { attributes } = props;
-	
+function EditorComp(props) {
+	const { attributes, onSelectMedia, setAttributes } = props;
+	const removeImage = (id) => {
+		const updatedGallery = attributes.gallery.filter((image) => image.mediaId !== id);
+		setAttributes({ gallery: updatedGallery });
+	};
+
 	return (
 		<div className={props.className}>
-			<em>Select or Remove Images from Sidebar</em>
-			<div class="editor-gallery">
-				{attributes.gallery  && attributes.gallery.map(media=><img key={media.mediaId} src={media.mediaUrl} />)}
-			</div>
+			<ul className="thumbnails">
+				{attributes.gallery &&
+					attributes.gallery.map((media) => (
+						media && <li key={media.mediaId}>
+							<div
+								className="remove"
+								onClick={() => removeImage(media.mediaId)}
+							>
+								X
+							</div>
+							<img src={media.mediaUrl} alt="Logo" />
+						</li>
+						
+					))}
+			</ul>
+
+			{attributes.gallery && <hr />}
+
+			<MediaUploadCheck>
+				<MediaUpload
+					onSelect={onSelectMedia}
+					allowedTypes={["image"]}
+					value={
+						attributes.gallery
+							? attributes.gallery.map((image) => image.mediaId)
+							: []
+					}
+					render={({ open }) => (
+						<Button onClick={open} isDefault isLarge className="centered">
+							{__("Add more photos", "GGL")}
+						</Button>
+					)}
+					multiple={true}
+				/>
+			</MediaUploadCheck>
 		</div>
 	);
 }
