5a03178dc6aed05d68e256c96389c0f0fcc9dc67
Gautam Jha <gautamjha.mailbox@gmail.com>
2021-09-04 11:15:02 +0530

    Final Custom Build

--- dist/helpers/custom.functions.js
+++ dist/helpers/custom.functions.js
@@ -19,7 +19,7 @@ function quoteButtonClickHandler(customElementName, $customIcon, item){
 			jQuery('.multi_qf-popup-show').trigger('click');
 			const _popup =jQuery('#multiQF_popup');
 			_popup.css({
-				'z-index':999999,
+				'z-index':9999999999,
 			});
 
 			_popup.find('h3#register').append(`<img class="img-quote" src="${item.src}" width="100" height="100"/><input value="${item.src}" name="image-quote-input" type="hidden" id="image-quote-input">`)
